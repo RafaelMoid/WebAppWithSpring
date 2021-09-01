1589bee7b095ad1a0e65a25214044d0f5e66587a
Rafael Varela <rafaelvrcr@gmail.com>
2021-09-01 13:16:09 -0300

    Criando o primeiro WEB app com Springboot

---
+++ src/main/java/com/example/TDSSpringTest/TdsSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.TDSSpringTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdsSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdsSpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TDSSpringTest/TdsSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TDSSpringTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdsSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
