88eb67d2184227e9f0edc3c02075809522070ff3
Rafael Varela <rafaelvrcr@gmail.com>
2021-09-01 16:06:19 -0300

    Criando as conexões entre author e books

--- src/main/java/com/example/TDSSpringTest/domain/Author.java
+++ src/main/java/com/example/TDSSpringTest/domain/Author.java
@@ -1,10 +1,22 @@
 package com.example.TDSSpringTest.domain;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+
+
+
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
+
     private String firstName;
     private String lastName;
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     public Author() {
@@ -14,6 +26,14 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -38,5 +58,20 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
--- src/main/java/com/example/TDSSpringTest/domain/Book.java
+++ src/main/java/com/example/TDSSpringTest/domain/Book.java
@@ -1,10 +1,23 @@
 package com.example.TDSSpringTest.domain;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
+
     private String title;
     private  String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+         inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
     public Book (){
@@ -15,6 +28,14 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
         this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -39,5 +60,20 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
